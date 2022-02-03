8e5e8df788cb9a008c93f04b3e572be1d39fa3e1
Sag3-Mode <westtmu@gmail.com>
2022-02-03 01:52:30 -0800

    first commit

---
+++ resources/views/users.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Laravel Page</title>
+</head>
+<body>
+<center>
+<h1 style="font-weight:600;font-size:100px; padding-top: 150px;">Laravel Project</h1>
+
+</center>
+    
+</body>
+</html>
